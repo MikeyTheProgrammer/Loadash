340d5a501fa385e3dc08e785933f8488dd85ad37
unknown <you@example.com>
2022-05-18 17:40:07 +0300

    shon did it all

--- code.js
+++ code.js
@@ -1,10 +1,34 @@
 const _ = require("lodash");
 //ex3
-const randomNumber = (min,max) => {
-   return  _.random(min,max); }
-   console.log(randomNumber(1,88))
+const randomNumber = (min, max) => {
+    return _.random(min, max);
+}
 
-   //ex4
+//ex4
+const minumber = (array) => {
+    return _.min(array)
+}
+
+//ex5
+const sumNumber = (array) => {
+    return _.sum(array)
+}
 
 
+//ex6
+const bigLetters = (array) => {
+    return _.upperCase([array])
+}
 
+//ex7
+const MLetters = (value) => {
+    return _.startsWith(value, "m")
+}
+
+        //ex8
+        const ObjLetters = (object) => {
+            return  _.keys(object) }
+            console.log(ObjLetters({
+                miki: "handsome",
+                shon: "adorable"
+            }));
